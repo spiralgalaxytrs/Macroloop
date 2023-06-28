3f09d5744960a70b5ba15144c5b3af88c90a5eb4
Rajkumar-2k21 <rajempire2k20@gmail.com>
2023-06-28 18:26:15 +0530

    blog change

--- src/components/blog-pages/blog/index.jsx
+++ src/components/blog-pages/blog/index.jsx
@@ -2,14 +2,14 @@ import BreadcrumbArea from "@/src/common/breadcrumb-area";
 import Footer from "@/src/layout/footers/footer";
 import Header from "@/src/layout/headers/header";
 import React from "react";
-import Blog from "../";
+import BlogArea from "./blog-area";
 
 const Blog = () => {
   return (
     <>
       <Header />
       <BreadcrumbArea acive_menu="Blog" title="News & Feeds" />
-      <Blog/>
+      <BlogArea />
       <Footer tp_border={true} />
     </>
   );
