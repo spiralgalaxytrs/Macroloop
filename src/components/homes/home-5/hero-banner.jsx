89813280ce21d1c4cd503a7a23aced0fa8bfe0c9
praghub <prakashbalan555@gmail.comgit config --global --listgit config --global user.email prakashbalan555@gmail.com>
2024-02-09 13:52:42 +0530

    update: about-area.jsx & hero-banner.jsx

--- src/components/homes/home-5/hero-banner.jsx
+++ src/components/homes/home-5/hero-banner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import  Link  from 'next/link';
+import Link from 'next/link';
 import Tilt from "react-parallax-tilt";
 import { TypeAnimation } from "react-type-animation";
 
@@ -10,11 +10,32 @@ const HeroBanner = () => {
     <>
       <div className="tp-da-hero pb-80 pt-80 dark-bg">
         <div className="container">
-          <div className="row">
+          <div className="row justify-content-between">
+            <div
+              className="col-lg-7 wow tpfadeUp"
+              data-tilt=""
+              data-tilt-perspective="2000"
+              data-wow-delay=".3s"
+            >
+              <Tilt
+                className="tilt-img"
+                tiltMaxAngleX={5}
+                tiltMaxAngleY={5}
+                perspective={900}
+                transitionSpeed={1000}
+                gyroscope={true}
+              >
+
+                <div className="tp-da-hero-img text-end pr-30">
+                  <img src="/assets/img/hero/da-hero-img.jpg" alt="" />
+                </div>
+
+              </Tilt>
+            </div>
             <div className="col-lg-5">
               <div className="da-hero-section mt-40">
                 <span className="da-hero-section__subtitle dark-bg mb-15 d-inline-block wow tpfadeUp">
-                  <b style={{color:"#044CBC"}}> LCG </b>
+                  <b style={{ color: "#044CBC" }}> LCG </b>
                 </span>
                 <div className="cd-intro">
                   <h4
@@ -22,10 +43,10 @@ const HeroBanner = () => {
                     data-wow-delay=".3s"
                   >
                     <span>Join in the movement of our Deep Tech Network
-</span>
+                    </span>
                     <span className="cd-words-wrapper text_style" >
 
-                    {/* <TypeAnimation
+                      {/* <TypeAnimation
                           sequence={[
                             'Convert.', 
                             1000,
@@ -54,31 +75,10 @@ const HeroBanner = () => {
                   </span>
                 </Link>
               </div>
-             
+
             </div>
 
-            <div
-              className="col-lg-7 wow tpfadeUp"
-              data-tilt=""
-              data-tilt-perspective="2000"
-              data-wow-delay=".3s"
-            >
-            <Tilt
-              className="tilt-img"
-              tiltMaxAngleX={5}
-              tiltMaxAngleY={5}
-              perspective={900}
-              transitionSpeed={1000}
-              gyroscope={true}
-              >
-           
-              <div className="tp-da-hero-img text-end pr-30">
-                <img src="/assets/img/hero/da-hero-img.jpg" alt="" />
-              </div>
-              
-            </Tilt>
-            </div>
-          
+
 
 
           </div>
