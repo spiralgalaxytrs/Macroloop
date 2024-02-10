bcab2bb6c722f267e9f0e2870b2dde045e0e4165
praghub <prakashbalan555@gmail.comgit config --global --listgit config --global user.email prakashbalan555@gmail.com>
2024-02-10 18:41:25 +0530

    upt: home-banner

--- src/components/homes/home-5/hero-slider.jsx
+++ src/components/homes/home-5/hero-slider.jsx
@@ -7,40 +7,40 @@ import { Swiper, SwiperSlide } from "swiper/react";
 const slider_content = [
   {
     id: 1,
-    bg_img: "/assets/img/slider/ptg-hero-1.jpg",
+    bg_img: "/assets/img/slider/BlogBanner.jpg",
     title: "Industry Automation",
   },
-  {
-    id: 2,
-    bg_img: "/assets/img/solutions/4.jpg",
-    title: "Smart campus solutions",
-  },
-  {
-    id: 3,
-    bg_img: "assets/img/solutions/5.jpg",
-    title: "IoT & Digital Transformation",
-  },
-  {
-    id: 4,
-    bg_img: "assets/img/slider/ptg-hero-4.jpg",
-    title: "Wifi solutions",
-  }, {
-    id: 5,
-    bg_img: "assets/img/slider/ptg-hero-5.jpg",
-    title: "AI-enabled software consultancy",
-  },
+  // {
+  //   id: 2,
+  //   bg_img: "/assets/img/solutions/4.jpg",
+  //   title: "Smart campus solutions",
+  // },
+  // {
+  //   id: 3,
+  //   bg_img: "assets/img/solutions/5.jpg",
+  //   title: "IoT & Digital Transformation",
+  // },
+  // {
+  //   id: 4,
+  //   bg_img: "assets/img/slider/ptg-hero-4.jpg",
+  //   title: "Wifi solutions",
+  // }, {
+  //   id: 5,
+  //   bg_img: "assets/img/slider/ptg-hero-5.jpg",
+  //   title: "AI-enabled software consultancy",
+  // },
 ];
 
 
 const HeroSlider = () => {
-    
+
   const [isLoop, setIsLoop] = useState(false);
   useEffect(() => {
     setIsLoop(true);
   }, []);
 
   const hero_content = {
-    // btn: "Hire Us Now",
+    btn: "Read More",
     // email: "info@webmail.com",
     // phone: "222 090 000 01",
   };
@@ -57,7 +57,7 @@ const HeroSlider = () => {
           }}
           className="swiper-container ptg-slider-active"
           effect="fade"
-          modules={[Autoplay,EffectFade, Navigation, Pagination]}
+          modules={[Autoplay, EffectFade, Navigation, Pagination]}
           pagination={{
             el: ".ptg-sldider-dot",
             clickable: true,
@@ -71,40 +71,34 @@ const HeroSlider = () => {
           {slider_content.map((item, i) => (
             <SwiperSlide
               key={i}
-              className="ptg-slider-item ptg-slider-height d-flex align-items-center p-relative"
+              className="ptg-slider-item d-flex align-items-center p-relative"
               data-background="assets/img/slider/ptg-hero-1.jpg"
               style={{
                 backgroundImage: `url(${item.bg_img})`,
+                height: "90vh"
               }}
             >
-              <div className="container">
-                <div className="row">
-                  <div className="col-12">
-                    <div className="tp-ptg-slider-content text-center p-relative">
-                      <h3
-                        className="tp-ptg-slider-title mb-45"
-                        data-animation="tpfadeUp"
-                        data-delay=".3s"
-                      >
-                        {/* {item.title}  */}
-                        <br />
-                      </h3>
-                      <Link
-                        href="/contact"
-                        className="ptg-slider-btn"
-                        data-animation="tpfadeUp"
-                        data-delay=".5s"
-                      >
-                        <span className="circle mr-20">
-                          <i  className="fal fa-long-arrow-right"></i>
+              <div className="container d-flex flex-column" style={{ height: "80vh"}}>
+                <div className="row flex-grow-1">
+                  <div className="col-lg-10 col-12 align-self-end">
+                    <h1 className="text-white display-4 fw-bolder">A LoopTech Venture: <br />LooperHub - A revolution in making.</h1>
+                  </div>
+                  <div className="col-lg-2 col-12 align-self-end">
+                    <div className="ptg-about-btn-wrapper mb-30 wow tpfadeUp" data-wow-delay=".3s">
+                      <Link href="/contact" className="tp-black-btn br-btn-bg-dark">
+                        {btn}
+                        <span className="ml-10">
+                          <i className="fal fa-long-arrow-right"></i>
                           <i className="fal fa-long-arrow-right"></i>
                         </span>
-                        MacroLoop Technologies Private Limited 
                       </Link>
                     </div>
                   </div>
                 </div>
               </div>
+
+
+
               {/* <div className="ptg-slider-social">
                 <b>Connect Here:</b>
                 <span>  <a href="#"> <i className="fab fa-facebook-f"></i> </a> </span>
