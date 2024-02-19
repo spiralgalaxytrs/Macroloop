5d682250e2f6149e4a1bdb068a11ef4f99d83074
RajkumarPalanichamy <rajempire2k20@gmail.com>
2024-02-20 00:29:43 +0530

    new  changes

--- src/components/job-details/job-details-area.jsx
+++ src/components/job-details/job-details-area.jsx
@@ -7,10 +7,13 @@ import Tilt from "react-parallax-tilt";
 
 
 const JobDetailsArea = ({data}) => {
-    console.log(data);
+    console.log(data?.list.id);
     
     return (
         <>
+        <style jsx> {'.ul{ list-style-type: square;}'}
+        
+        </style>
             <div className="tp-job-details pt-120 dark-bg">
          <div className="container">
          <div className="row">
@@ -34,26 +37,82 @@ const JobDetailsArea = ({data}) => {
                <div className="row justify-content-center">
                   <div className="col-lg-10">
                      <div className="tp-inner-pt-section wow tpfadeUp">
-                        <h4 className="tp-inner-pt-section__title text-white mb-30">{data?.title}</h4>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title}</h4>
                         <p className="mb-30">
                             {data?.d1}
-                        </p><p className="mb-30">
+                           
+                        </p>
+                       
+                        
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title1}</h4>
+                        <p className="mb-30">
                             {data?.d2}
-                        </p><p className="mb-30">
+                        </p>
+                        <div className='ul'>
+                        {data?.list.map((e)=>(
+                         
+                         <ul><li>
+                           
+                        {e.id}
+                            </li></ul> 
+                        
+                        )
+                        )}
+                        </div>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title2}</h4>
+                        <p className="mb-30">
                             {data?.d3}
-                        </p><p className="mb-30">
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title3}</h4>
+                        <p className="mb-30">
                             {data?.d4}
-                        </p><p className="mb-30">
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title4}</h4>
+                        <p className="mb-30">
                             {data?.d5}
                         </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title5}</h4>
                         <p className="mb-30">
                             {data?.d6}
                         </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title6}</h4>
+                        <p className="mb-30">
+                            {data?.d7}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title7}</h4>
+                        <p className="mb-30">
+                            {data?.d8}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title8}</h4>
+                        <p className="mb-30">
+                            {data?.d9}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title9}</h4>
+                        <p className="mb-30">
+                            {data?.d10}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title10}</h4>
+                        <p className="mb-30">
+                            {data?.d11}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title11}</h4>
+                        <p className="mb-30">
+                            {data?.d11}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title12}</h4>
+                        <p className="mb-30">
+                            {data?.d12}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title13}</h4>
+                        <p className="mb-30">
+                            {data?.d13}
+                        </p>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">{data?.title14}</h4>
                         
 
                       
                        
-                        <h4 className="tp-inner-pt-section__title text-white mb-30">Contact Us</h4>
+                        <h4 className="tp-inner-pt-section__title text-black mb-30">Contact Us</h4>
 
                         <div className="tp-inner-pg-form wow tpfadeUp" data-wow-delay=".4s">
                   <div className="it-cta-form about-cta-form wow tpfadeUp">
