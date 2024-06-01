e628d90a2d94b912c7022de066dd931a975f525b
Vaseekaran-S <vaseekaransaminathan@gmail.com>
2024-06-01 17:01:57 +0530

    fix: slider title & color gradient

--- src/components/homes/home-5/hero-slider.jsx
+++ src/components/homes/home-5/hero-slider.jsx
@@ -9,31 +9,37 @@ const slider_content = [
     id: 1,
     bg_img: "/assets/img/slider/BlogBanner.jpg",
     url: "/LooperHub",
+    title: <>A LoopTech Venture: <br/> LooperHub - A revolution in making.</>
   },
   {
     id: 2,
     bg_img: "/assets/img/solutions/construction.gif",
     url: "/solutions/Construction-Technical",
+    title: <>Solutions : <br /> Construction Technology</>
   },
   {
     id: 3,
     bg_img: "/assets/img/solutions/DigT.gif",
     url: "/solutions/Digital-Transformation",
+    title: <>Solutions : <br /> Digital Transformation</>
   },
   {
     id: 4,
     bg_img: "/assets/img/solutions/ed.gif",
     url: "/solutions/Edu-Tech",
+    title: <>Solutions : <br /> Edu-Tech</>
   },
   {
     id: 5,
     bg_img: "/assets/img/solutions/system-solutions.gif",
     url: "/solutions/Security-Systems",
+    title: <>Solutions : <br /> Security Systems</>
   },
   {
     id: 6,
     bg_img: "/assets/img/solutions/IT-infrastructure.gif",
     url: "/solutions/Infrastructure",
+    title: <>Solutions : <br /> IT Infrastructure</>
   }
 ];
 
@@ -58,7 +64,7 @@ const HeroSlider = () => {
           slidesPerView={1}
           loop={true}
           autoplay={{
-            delay: 2500,
+            delay: 5000,
             disableOnInteraction: false,
           }}
           className="swiper-container ptg-slider-active"
@@ -87,7 +93,7 @@ const HeroSlider = () => {
               <div className="container d-flex flex-column slider_content" >
                 <div className="row pt-100">
                   <div className="col-lg-10 col-12 align-self-end">
-                    <h1 className="text-white display-4 fw-bolder">A LoopTech Venture: <br />LooperHub - A revolution <br /> in making.</h1>
+                    <h1 className="text-white display-4 fw-bolder">{item?.title}</h1>
                   </div>
                   <div className="self-end pt-10 readMore d-flex flex-row-reverse">
                     <div className="ptg-about-btn-wrapper mb-30 wow tpfadeUp" data-wow-delay=".3s">
