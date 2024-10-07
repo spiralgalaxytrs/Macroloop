3bfadd298fa4b4cddff81ecbbe30c231e16fc16c
shankar <shankartrs02@gmail.com>
2024-10-07 10:05:13 +0000

    header footer changes

--- src/layout/headers/menu-data.js
+++ src/layout/headers/menu-data.js
@@ -17,32 +17,53 @@ const menu_data = [
     id: 3,
     mega_menu: false,
     has_dropdown: true,
-    sub_menus : [{
-      title : "CONSTRUCTION TECHNOLOGY",
-      link : "/solutions/Construction-Technical"
+    sub_menus: [{
+      title: "CONSTRUCTION TECHNOLOGY",
+      link: "/solutions/Construction-Technical"
     },
     {
       title: "DIGITAL TRANSFORMATION",
-      link : "/solutions/Digital-Transformation"
+      link: "/solutions/Digital-Transformation",
+      sub_menus: [
+        {
+          title: "IOT SOLUTION",
+          link: "solutions"
+        },
+        {
+          title: "ROBOTIC AUTOMATION",
+          link: "solutions"
+        },
+        {
+          title: "METAVERSE SOLUTION",
+          link: "solutions"
+        },
+        {
+          title: "SECURITY SYSTEMS",
+          link: "solutions/Digital-Transformation/Security-Systems"
+        },
+        {
+          title: "IT INFRASTRUCTURE",
+          link: "solutions/Digital-Transformation/Infrastructure"
+        }]
     },
     {
-      title : "EDU-TECH",
-      link : "/solutions/Edu-Tech"
+      title: "EDU-TECH",
+      link: "/solutions/Edu-Tech"
     },
-    {
-      title : "SECURITY SYSTEMS",
-      link : "/solutions/Security-Systems"
-    },
-    {
-      title : "IT INFRASTRUCTURE",
-      link : "/solutions/Infrastructure"
-    },
+      // {
+      //   title : "SECURITY SYSTEMS",
+      //   link : "/solutions/Security-Systems"
+      // },
+      // {
+      //   title : "IT INFRASTRUCTURE",
+      //   link : "/solutions/Infrastructure"
+      // },
     ],
     title: "Our Solutions",
     link: "/solutions",
-    
+
   },
- 
+
   {
     id: 4,
     mega_menu: false,
@@ -56,7 +77,7 @@ const menu_data = [
     has_dropdown: false,
     title: "LCG",
     link: "/lcg",
-   
+
   },
   {
     id: 6,
